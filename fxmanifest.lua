60e46f5606aa7ecd896fb733139ecaba19c63ee2
ExploreNetwork <63176954+ExploreNetwork@users.noreply.github.com>
2024-04-14 18:14:11 +1000

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'ExploreNetwork'
+description 'An autopilot helicopter that can track suspects on foot or in vehicle and provide radio intelligence to officers.'
+version '1.0.0'
+
+client_script 'mymode_client.lua'
